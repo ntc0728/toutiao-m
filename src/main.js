737864abd30de218f5034e7b175527041a28e493
ntc0728 <1343810646@qq.com>
2020-07-28 19:56:27 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载 Vant 全局样式
+import Vant from 'vant'
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
 
 // 加载全局样式
-import './styles/index.less'
 
+import './styles/index.less'
+// 注册使用 Vant 组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
